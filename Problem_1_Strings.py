6840b4324ffa7d339cb8b54f84bc3a03d623222d
Zack Khalidov <46911135+zkhalido@users.noreply.github.com>
2020-02-15 18:43:04 -0700

    Add files via upload

---
+++ Problem_1_Strings.py
@@ -0,0 +1,9 @@
+"""
+This program takes a string and prints the reverse of the string.
+
+Problem #1 CS1030
+author: Zack Khalidov
+"""
+
+string = input("Enter a string to print the reverse: ")
+print(string[::-1])
